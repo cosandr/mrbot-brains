5520a550350e35449e1ba2fe6936b2e09d3da1e2
Andrei Costescu <andrei@costescu.no>
2020-11-10 19:18:26 +0100

    Refactor Response
    Use a dict to hold attributes to make extending and serialization easier

--- ext/response.py
+++ ext/response.py
@@ -1,31 +1,54 @@
 import json
+from typing import Union
 
 from aiohttp import web
 
 
 class Response:
-    def __init__(self, data=None, params=None, error='', time=0):
+    def __init__(self, **kwargs):
         self._web = web.Response(content_type="application/json")
-        # An error string
-        self.error: str = error
-        # How long the request took to complete
-        self.time: float = time
-        # Return data, probably a dict or string
-        self.data = data
-        # The parameters used by the function that was run
-        self.params: dict = params
+        self._attrs = dict(
+            data=None,    # Return data, probably a dict or string
+            error='',     # An error string
+            params=None,  # The parameters used by the function that was run
+            time=0,       # How long the request took to complete
+        )
+        self._attrs.update(kwargs)
+
+    @property
+    def data(self) -> Union[str, dict, list]:
+        return self._attrs.get('data', None)
+
+    @data.setter
+    def data(self, val: Union[str, dict, list]):
+        self._attrs['data'] = val
+
+    @property
+    def error(self) -> str:
+        return self._attrs.get('error', '')
+
+    @error.setter
+    def error(self, val: str):
+        self._attrs['error'] = val
+
+    @property
+    def params(self) -> dict:
+        return self._attrs.get('params', {})
+
+    @params.setter
+    def params(self, val: dict):
+        self._attrs['params'] = val
+
+    @property
+    def time(self) -> dict:
+        return self._attrs.get('time', 0)
+
+    @time.setter
+    def time(self, val: float):
+        self._attrs['time'] = val
 
     def to_json(self) -> str:
-        send_dict = {}
-        if self.data:
-            send_dict["data"] = self.data
-        if self.params:
-            send_dict["params"] = self.params
-        if self.error:
-            send_dict["error"] = self.error
-        if self.time:
-            send_dict["time"] = self.time
-        return json.dumps(send_dict)
+        return json.dumps({k: v for k, v in self._attrs.items() if v})
 
     @property
     def status(self) -> int:
@@ -38,9 +61,9 @@ class Response:
     @property
     def web_response(self) -> web.Response:
         """Return internal web response"""
-        self._web.text = self.to_json()
+        self._web.body = self.to_json().encode('utf-8')
         return self._web
 
     def to_web(self, **kwargs) -> web.Response:
         """Return aiohttp web response, keyword args are passed to web.Response constructor"""
-        return web.Response(text=self.to_json(), content_type="application/json", **kwargs)
+        return web.Response(body=self.to_json().encode('utf-8'), content_type="application/json", **kwargs)
